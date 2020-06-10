cbc64e5d2ea921d1d900e87c001a9d77bd01f4a8
turisap <kirillshakirov57@gmail.com>
2020-06-10 22:40:51 +0300

    loading font faces using FontFace API

--- src/INIT.ts
+++ src/INIT.ts
@@ -1,5 +1,28 @@
-// setup performance observer
+// load fonts
+const fontFaces = [
+  ["OpenSans-Bold", "600"],
+  ["OpenSans-SemiBold", "500"],
+  ["OpenSans-Light", "100"],
+  ["OpenSans-Regular", "300"],
+];
 
+fontFaces
+  .map(
+    ([font, weight]) =>
+      new FontFace("Open Sans", `url(../assets/fonts/${font}.ttf)`, {
+        style: "normal",
+        unicodeRange: "U+000-5FF",
+        weight,
+      })
+  )
+  .forEach((fontFace) => {
+    fontFace.load().then(function() {
+      document.fonts.add(fontFace);
+      document.body.style.fontFamily = "Open Sans, serif";
+    });
+  });
+
+// setup performance observer
 if (typeof PerformanceObserver !== "undefined") {
   const observer = new PerformanceObserver((list) => {
     const perfEntries = list.getEntries();
@@ -13,6 +36,11 @@ if (typeof PerformanceObserver !== "undefined") {
   observer.observe({ entryTypes: ["longtask"] });
 }
 
-const { saveData, effectiveType } = window.navigator["connection"];
-// TODO add GA
-console.log("Save data %s, type %s", saveData, effectiveType);
+// check user connection
+if (typeof navigator !== "undefined") {
+  const saveData = (window.navigator as any)?.connection?.saveData;
+  const effectiveType = (window.navigator as any)?.connection?.effectiveType;
+
+  // TODO add GA
+  console.log("Save data %s, type %s", saveData, effectiveType);
+}
