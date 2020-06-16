8623e72ee3de4435a502900d21917293aec30e52
turisap <kirillshakirov57@gmail.com>
2020-06-16 21:35:38 +0300

    add base_endpoint

--- src/redux/ducks/graphs.ts
+++ src/redux/ducks/graphs.ts
@@ -3,12 +3,13 @@ import { takeLatest, call, put, fork } from "redux-saga/effects";
 import LogRocket from "logrocket";
 
 import { API } from "types/";
-import { actionPrefixer, asyncActionPrefixer } from "utils/";
+import { asyncActionPrefixer } from "utils/";
 import { get } from "requestBuilder";
 
 import { ioTSLogger } from "../../utils";
 import { enqueueNotification } from "./notifications";
 
+const BASE_ENDPOINT = "/graph_data";
 const DUCK_PREFIX = "graphs";
 
 const pra = asyncActionPrefixer(DUCK_PREFIX);
@@ -38,7 +39,7 @@ const graphsReducer = createReducer<API.GraphData>(DEFAULT).handleAction(
 
 function* getGraphData() {
   try {
-    const result: API.GraphData = yield call(get, "/graph_data");
+    const result: API.GraphData = yield call(get, BASE_ENDPOINT);
 
     ioTSLogger(API.GraphData, result, "fetch graph data");
 
