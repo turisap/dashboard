ff226e4c68cca73e271a7a8c29ed37435d090e0a
turisap <kirillshakirov57@gmail.com>
2020-06-30 00:20:36 +0300

    add global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,7 +2,6 @@ declare module "react-hot-loader/root";
 declare module "react-apexcharts";
 declare module "react-icons/ai";
 declare module "react-icons/ti";
-declare module "promise-timeout";
 
 export {};
 
